71da1966fbb865a19ad624c358f84340a1158c5f
Rudolf Schlenger <schlengerrudolfde@gmail.com>
2026-01-12 16:20:44 +0100

    Add winning and tie check

--- ticTacToe.js
+++ ticTacToe.js
@@ -33,6 +33,7 @@ function player(name) {
 }
 
 const gameController = (function () {
+    let roundCounter = 1;
     const playerOne = player("Rudi").getName();
     const playerTwo = player("Diego").getName();
 
@@ -44,20 +45,83 @@ const gameController = (function () {
 
     const playRound = function (row, column) {
         let validTurn = false;
-        console.log(validTurn);
 
         while (!validTurn) {
             validTurn = gameBoard.setField(row, column, playersTurn);
-            console.log(validTurn);
         }
 
+        if (roundCounter >= 5 && checkWin(playersTurn)) {
+            console.log(playersTurn + " is the winner!");
+            return;
+        }
+        if (roundCounter == 9) {
+            console.log("It's a tie!")
+            return;
+        }
         switchTurn();
+        roundCounter++;
+    }
+    
+    const checkWin = function (player) {
+        let isWinner;
+
+        //Check vertical win condition
+        for (let i = 0; i < gameBoard.getBoard.length; i++) {
+            for (let j = 0; j < gameBoard.getBoard.length; j++) {
+                if (player !== gameBoard.getBoard()[i][j]) {
+                    isWinner = false;
+                    break;
+                }
+                else {
+                    isWinner = true;
+                }
+            }
+            if (isWinner) return true;
+        }
+
+        //Check horizontal win condition
+        for (let i = 0; i < gameBoard.getBoard.length; i++) {
+            for (let j = 0; j < gameBoard.getBoard.length; j++) {
+                if (player !== gameBoard.getBoard()[j][i]) {
+                    isWinner = false;
+                    break;
+                }
+                else {
+                    isWinner = true;
+                }
+            }
+            if (isWinner) return true;
+        }
+
+        //Check top-left diagonal win condition
+        for (let i = 0; i < gameBoard.getBoard.length; i++) {
+            if (player !== gameBoard.getBoard()[i][i]) {
+                isWinner = false;
+                break;
+            }
+            isWinner = true;
+        }
+        if (isWinner) return true;
+
+        //Check top-right diagonal win condition
+        for (let i = 2; i >= 0; i--) {
+                if (player !== gameBoard.getBoard()[i][2 - i]) {
+                    return false;
+                }
+        }
+        return true;
     }
 
     return { playRound };
 })();
 
-gameController.playRound(0, 0);
+gameController.playRound(0, 1);
+gameController.playRound(0, 2);
 gameController.playRound(1, 1);
+gameController.playRound(2, 1);
 gameController.playRound(2, 2);
-console.log(gameBoard.getBoard())
+gameController.playRound(0, 0);
+gameController.playRound(1, 0);
+gameController.playRound(1, 2);
+gameController.playRound(2, 0);
+console.log(gameBoard.getBoard());
